df9ff856ef54166c603ff6ac76c4f78950943358
Aliaksei <aliaksei@immersive.cz>
2022-01-04 23:16:40 +0100

    add error page remove unused assets

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const tasksRouter = require('./routes/tasks');
 const loginRouter = require('./routes/login');
 const db = require('./database');
@@ -24,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 app.use('/login', loginRouter);
 
@@ -35,13 +32,7 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error', {message: res.locals.message, error: res.locals.error});
+    res.render('error',{tittle: "Page not found",message:`Page ${req.url} not found`, })
 });
 db.initDB();
 
